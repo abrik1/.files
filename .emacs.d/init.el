f7e27ea88728aec0dea6636b2c64e76545ee1955
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-07-03 23:26:36 +0530

    emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,34 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruvbox-light-medium))
+ '(custom-safe-themes
+   '("98ef36d4487bf5e816f89b1b1240d45755ec382c7029302f36ca6626faf44bbd" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "ba323a013c25b355eb9a0550541573d535831c557674c8d59b9ac6aa720c21d3" "871b064b53235facde040f6bdfa28d03d9f4b966d8ce28fb1725313731a2bcc8" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" default))
+ '(minimap-minimum-width 10)
+ '(minimap-mode t)
+ '(minimap-window-location 'right)
+ '(package-selected-packages
+   '(minimap tree-sitter-langs doom-modeline gruvbox-theme rainbow-delimiters rainbow-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'doom-modeline)
+(doom-modeline-mode 1)
+
+(setq doom-modeline-icon nil)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(set-frame-font "ComicShannsMono Nerd Font" nil t)
+(global-display-line-numbers-mode)
+(scroll-bar-mode -1)
