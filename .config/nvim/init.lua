490234b937d2ff5e452ccf39aee0aee83c91cf9a
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-08-31 17:33:44 +0530

    add lualine and rainbow-delimeters config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,45 @@
+local lualine_options = {
+   icons_enabled = false;
+   component_separators = { left = '|', right = '|'};
+   section_separators = {};
+}
+
+local lualine_sections = {
+   lualine_a = {'mode'},
+   lualine_b = {'filename'},
+   lualine_c = {},
+   lualine_x = {},
+   lualine_y = {'progress'},
+   lualine_z = {'location'},
+}
+
 require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query" , "python"},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  -- ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
   highlight = {
     enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    --
+    -- the name of the parser)
+    -- list of language that will be disabled
     disable = { "c", "rust" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -12,9 +47,15 @@ require'nvim-treesitter.configs'.setup {
             return true
         end
     end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
 }
+
 
 local ensure_packer = function()
   local fn = vim.fn
@@ -42,6 +83,7 @@ lsp_defaults.capabilities = vim.tbl_deep_extend(
 )
 
 lspconfig.pylsp.setup({})
+lspconfig.arduino_language_server.setup({})
 
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
@@ -63,8 +105,17 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-path'
-  use 'hr7th/cmp-buffer'
+  use 'hrsh7th/cmp-buffer'
+  use 'alec-gibson/nvim-tetris'
+  use 'HiPhish/rainbow-delimiters.nvim'
+  use 'nvim-lualine/lualine.nvim'
 
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
   if packer_bootstrap then
     require('packer').sync()
   end
@@ -81,6 +132,7 @@ return require('packer').startup(function(use)
       ['<C-Space>'] = cmp.mapping.complete(),
    },
 
+  -- You should specify your *installed* sources.
   sources = {
     { name = 'buffer' },
     { name = 'nvim_lsp' },
@@ -88,8 +140,34 @@ return require('packer').startup(function(use)
   },
  })
 
+  require('lualine').setup{options=lualine_options, sections=lualine_sections}
+
+  -- This module contains a number of default definitions
+  local rainbow_delimiters = require 'rainbow-delimiters'
+
+  vim.g.rainbow_delimiters = {
+    strategy = {
+        [''] = rainbow_delimiters.strategy['global'],
+        vim = rainbow_delimiters.strategy['local'],
+    },
+    query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-blocks',
+    },
+    highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+    },
+  }
+
   vim.cmd [[colorscheme tokyonight-moon]]
   vim.cmd [[syntax on]]
   vim.cmd [[set number]]
   vim.cmd [[set relativenumber]]
+  vim.diagnostic.disable()
 end)
